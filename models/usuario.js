bb1861f99c93a3b20b81c6aee49bf93a6e4afd42
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-12-25 15:57:31 -0300

    creo el modelo de usuario

---
+++ models/usuario.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const UsuarioSchema = Schema({
+  nombre: {
+    type: String,
+    required: [true, "Este dato es obligatorio!"],
+  },
+  correo: {
+    type: String,
+    required: [true, "Este dato es obligatorio!"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Este dato es obligatorio!"],
+  }
+});
+
+//Quitar datos de la respuesta
+UsuarioSchema.methods.toJSON = function () {
+  const { password, __v, ...usuario } = this.toObject();
+
+  return usuario;
+};
+
+module.exports = model("Usuario", UsuarioSchema);
